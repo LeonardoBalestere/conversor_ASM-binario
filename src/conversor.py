454337e79509b8a6f049f3d05f493d99ddcf6cda
Leonardo Balestere <alvesleonardo92@hotmail.com>
2022-02-17 20:11:51 -0300

    Criado função processaPonto e iniciaProcessoConversão, adicionado também arquivo para testes

--- src/conversor.py
+++ src/conversor.py
@@ -1,9 +1,29 @@
+def processaPonto(linhaPonto):
+    linhaPonto = linhaPonto.strip()
+    if linhaPonto[:5] == '.text':
+        return 1
+    return 0
+
+
+def iniciaProcessoConversão(linhas, linhaAposPonto):
+    contadorLinhas = 0
+    for linhaAtual in linhas:
+        contadorLinhas += 1
+        if contadorLinhas < linhaAposPonto:
+            break
+
+    #A partir daqui ele será e interpretará o código, pois está após o .text
+
+
 nomeArquivo = input('Digite o nome do arquivo: ')
 with open(nomeArquivo, 'r') as conteúdoArquivo:
     linhas = conteúdoArquivo.readlines()
-
 for linha in linhas:
+    linhaAposPonto = 0
     for caracter in linhas:
+        linhaAposPonto += 1
         while caracter != '.':
             break
-        processaPonto(linha)
+
+        if processaPonto(linha) == 1:
+            retorno = iniciaProcessoConversão(linhas, linhaAposPonto)
