c650b75190a1cbf93943dd9606fa5e452fbedc63
LeonardoBalestere <alvesleonardo92@hotmail.com>
2022-02-24 17:42:57 -0300

    Adicionado como será as últimas funções

--- src/conversor.py
+++ src/conversor.py
@@ -1,3 +1,21 @@
+def tipoJ(instrução):
+    #TODO
+    #Adicionar instrução do tipo Jump, que possui apenas o opcode e o endereço
+    return
+
+
+def tipoRR(instrução):
+    #TODO
+    #Adicionar instrução normal, que possui a instrução seguida de 3 registradores
+    return
+
+
+def tipoRI(instrução):
+    #TODO
+    #Adicionar instrução que possui o registrador e uma variável do tipo inteiro
+    return
+
+
 def ehText(linhaPonto):
     linhaPonto = linhaPonto.strip().lower()
     if linhaPonto[:5] == '.text':
--- src/testes.py
+++ src/testes.py
@@ -1,5 +1,16 @@
-l = 'addi	$v0, $zero, 5'
+def tipoJ(instrução):
+    #TODO
+    #Adicionar instrução do tipo Jump, que possui apenas o opcode e o endereço
+    return
 
-l = l.split()
 
-print(l)
+def tipoRR(instrução):
+    #TODO
+    #Adicionar instrução normal, que possui a instrução seguida de 3 registradores
+    return
+
+
+def tipoRI(instrução):
+    #TODO
+    #Adicionar instrução que possui o registrador e uma variável do tipo inteiro
+    return
